e3f7d80dfb9f80bb9c2cf34607940df1b923a533
mainurrahat <mainurrahat51@gmail.com>
2025-11-05 14:20:30 +0600

    square Clicked

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import Square from "./Square";
+import Square from "./Square.jsx";
 
 export default function Board() {
   return (
     <div className="grid grid-cols-3 gap-0.5 w-max mx-auto mt-4">
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
+      <Square Value="1"></Square>
+      <Square Value="2"></Square>
+      <Square Value="3"></Square>
+      <Square Value="4"></Square>
+      <Square Value="5"></Square>
+      <Square Value="6"></Square>
+      <Square Value="7"></Square>
+      <Square Value="8"></Square>
+      <Square Value="9"></Square>
     </div>
   );
 }
--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-
-const Square = () => {
+function hanleClick() {
+  alert("Square clicked!");
+}
+const Square = ({ Value }) => {
   return (
     <div>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-        X
+      <button
+        onClick={hanleClick}
+        className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      >
+        {Value}
       </button>
     </div>
   );
